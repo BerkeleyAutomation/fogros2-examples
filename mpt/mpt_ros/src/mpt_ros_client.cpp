cc14f947fa9e62b75674e0cf9567ac39b152637d
Eric Chen <kych@berkeley.edu>
2023-09-14 20:29:10 +0000

    separate cost and time

--- mpt/mpt_ros/src/mpt_ros_client.cpp
+++ mpt/mpt_ros/src/mpt_ros_client.cpp
@@ -69,6 +69,8 @@ class MinimalPublisher : public rclcpp::Node
     {
     // ros::param::param<std::string>("scenario", default_param, "Apartment");
     this->declare_parameter("scenario", rclcpp::PARAMETER_STRING);
+    this->declare_parameter("experiment", rclcpp::PARAMETER_STRING);
+    // this->set_parameter(rclcpp::Parameter("experiment", "latency"));
     this->envMeshPub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("environment_mesh", 1000);
     this->robotMeshPub = this->create_publisher<std_msgs::msg::UInt8MultiArray>("robot_mesh", 1000);
     this->motionPlanRequestPub = this->create_publisher<mpt_ros::msg::MotionPlanRequest>("motion_plan_request", 1000);
@@ -83,8 +85,18 @@ class MinimalPublisher : public rclcpp::Node
         "motion_plan", 1000,
         std::bind(&MinimalPublisher::motionPlanCallback, this, _1));
     
-    timer_ = this->create_wall_timer(
-      16s, std::bind(&MinimalPublisher::publish_callback, this));
+
+    rclcpp::Parameter str_param = this->get_parameter("experiment");
+    // print experiment type variable 
+    RCLCPP_INFO(this->get_logger(), "experiment type: %s", str_param.as_string().c_str());
+    // use publish cost of optimizer is cost 
+    if (str_param.as_string() == "latency")
+        timer_ = this->create_wall_timer(
+             20s, std::bind(&MinimalPublisher::publish_callback_latency, this));
+    else 
+        timer_ = this->create_wall_timer(
+            20s, std::bind(&MinimalPublisher::publish_callback_cost, this));
+
         RCLCPP_INFO(this->get_logger(), "Initing");
         RCLCPP_INFO(this->get_logger(), "Entering loop");
     }
@@ -119,7 +131,7 @@ class MinimalPublisher : public rclcpp::Node
     }
 
 
-    void publish_callback() {
+    void publish_callback_cost() {
         gotPlan_ = false;
         RCLCPP_INFO(this->get_logger(), "Sending env");
         mpt_ros::msg::MotionPlanRequest req;
@@ -157,7 +169,119 @@ class MinimalPublisher : public rclcpp::Node
                 -1.0, -1.0;
             // We target a cost of 650, which can take from 20 s to
             // over 10 min on 1 core.
-            req.min_solution_cost = 650;
+            req.min_solution_cost = 1;
+        } else if (scenario == "Home") {
+            auto env = sendMesh( "Home_env.dae");
+            auto robot = sendMesh( "Home_robot.dae");
+            envMeshPub->publish(env);
+            robotMeshPub->publish(robot);
+            bounds <<
+                -383.802642822, 324.997131348,
+                -371.469055176, 337.893371582,
+                -0.196851730347, 142.332290649;
+            startAndGoal <<
+                252.95, 262.95,
+                -214.95, 75.05,
+                46.19, 46.19,
+                0, 0,
+                1, 1,
+                0, 0,
+                0, 0;
+            req.min_solution_cost = 1;
+        } else if (scenario == "cubicles") {
+            auto env = sendMesh("cubicles_env.dae");
+            auto robot = sendMesh("cubicles_robot.dae");
+            this->envMeshPub->publish(env);
+            this->robotMeshPub->publish(robot);
+            bounds <<
+          -508.88, 319.62,
+          -230.13, 531.87,
+          -123.75, 101.0;
+            startAndGoal <<
+          -4.96, 200,
+          -40.62, -40.62,
+          70.57, 70.57,
+                0, 0,
+                1, 1,
+                0, 0,
+                0, 0;
+            req.min_solution_cost = 1;
+        } else if (scenario == "Twistycool") {
+            // Ug, this seems to have a solution that is just to
+            // interpolate from start to goal.
+            auto env = sendMesh( "Twistycool_env.dae");
+            auto robot = sendMesh( "Twistycool_robot.dae");
+            envMeshPub->publish(env);
+            robotMeshPub->publish(robot);
+            bounds <<
+          53.46, 402.96,
+          -21.25, 269.25,
+          -476.86, -91;
+            startAndGoal <<
+          270, 270,
+          160,160,
+          -200, -400,
+                0, 0,
+                1, 1,
+                0, 0,
+                0, 0;
+        } else {
+            throw std::invalid_argument("unknown scenario: " + scenario);
+        }
+        std::this_thread::sleep_for(2s);
+        startTime = Clock::now();
+        for (int i=0 ; i<3 ; ++i)
+            req.bounds_min.push_back(bounds(i, 0));
+        for (int i=0 ; i<3 ; ++i)
+            req.bounds_max.push_back(bounds(i, 1));
+        for (int i=0 ; i<7 ; ++i)
+            req.start_config.push_back(startAndGoal(i, 0));
+        for (int i=0 ; i<7 ; ++i)
+            req.goal_config.push_back(startAndGoal(i, 1));
+        // sendMatrix(boundsPub, bounds);
+        // sendMatrix(planPub, startAndGoal);
+        motionPlanRequestPub->publish(req);
+    }
+
+    void publish_callback_latency() {
+        gotPlan_ = false;
+        RCLCPP_INFO(this->get_logger(), "Sending env");
+        mpt_ros::msg::MotionPlanRequest req;
+        Eigen::Matrix<double, 3, 2> bounds;
+        Eigen::Matrix<double, 7, 2> startAndGoal;
+
+        req.max_planning_time = 300.0;
+        req.min_planning_time = 0;
+        req.min_solution_cost = std::numeric_limits<double>::infinity();
+
+        req.start_names = std::vector<std::string>(
+            {{ "tx","ty","tz", "angle", "axis.x", "axis.y", "axis.z" }});
+        req.goal_names = req.start_names;
+        req.bounds_names = std::vector<std::string>(
+            {{ "tx", "ty", "tz" }});
+        std::string scenario = "cubicles"; //Apartment, Home, cubicles, Twistycool, ;
+        rclcpp::Parameter str_param = this->get_parameter("scenario");
+        scenario = str_param.as_string();
+        if (scenario == "Apartment") {
+            auto env = sendMesh("Apartment_env.dae");
+            auto robot = sendMesh("Apartment_robot.dae");
+            envMeshPub->publish(env);
+            robotMeshPub->publish(robot);
+            bounds <<
+                -73.76, 295.77,
+                -179.59, 168.26,
+                -0.03, 90.39;
+            startAndGoal <<
+                241.81,   -31.19,
+                106.15,   -99.85,
+                36.46,    36.46,
+                3.12413936107, 3.12413936107,
+                0.0, 0.0,
+                0.0, 0.0,
+                -1.0, -1.0;
+            // We target a cost of 650, which can take from 20 s to
+            // over 10 min on 1 core.
+            req.min_solution_cost = 800;
         } else if (scenario == "Home") {
             auto env = sendMesh( "Home_env.dae");
             auto robot = sendMesh( "Home_robot.dae");
@@ -213,6 +337,7 @@ class MinimalPublisher : public rclcpp::Node
                 1, 1,
                 0, 0,
                 0, 0;
+            req.min_solution_cost = 2800;
         } else {
             throw std::invalid_argument("unknown scenario: " + scenario);
         }
